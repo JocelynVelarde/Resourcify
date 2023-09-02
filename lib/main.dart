40d7b78c94c26fc1a2965d2d57147483d21256a4
mar-hz <103940408+mar-hz@users.noreply.github.com>
2023-09-02 14:49:09 -0600

    test 2?

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/loginregister.dart';
 import 'package:flutter_application_1/onboarding.dart';
-import 'widget_tree.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
@@ -11,7 +9,7 @@ Future<void> main() async {
 }
 
 class Main extends StatelessWidget {
-  const Main({key});
+  const Main({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +17,7 @@ class Main extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: OnboardingScreen(),
+      home: const OnboardingScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
