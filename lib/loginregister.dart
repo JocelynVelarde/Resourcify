d04e1fbd6c7a025775fee7eaf2496659472affcb
mar-hz <103940408+mar-hz@users.noreply.github.com>
2023-09-02 11:13:06 -0600

    onboarding gradient

--- lib/loginregister.dart
+++ lib/loginregister.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'auth.dart';
-import 'home.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
